d16691a3967090a0f2cf715e528c627c47785a24
Kyle Barron <kylebarron2@gmail.com>
2022-11-26 16:36:54 -0700

    somewhat working code block

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { runEntireFile, runInferredCodeBlock } from "./run-code";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,14 +13,20 @@ export function activate(context: vscode.ExtensionContext) {
     'Congratulations, your extension "vscode-jupyter-python" is now active!'
   );
 
+  const tmp = vscode.workspace.getConfiguration("vscode-jupyter-python");
+  const expandCode = tmp.get("expandCode");
+
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("vscode-jupyter-python.sayHello", () => {
+  let disposable = vscode.commands.registerCommand("vscode-jupyter-python.sayHello", async () => {
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from vscode-jupyter-python!");
+    // vscode.window.showInformationMessage(`Hello World ${expandCode} from vscode-jupyter-python!`);
+    // await vscode.commands.executeCommand('jupyter.execSelectionInteractive', "print('sent to Interactive Window by package')\n");
+    runInferredCodeBlock();
   });
 
   context.subscriptions.push(disposable);
--- src/run-code.ts
+++ src/run-code.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
-import { TextDocument, Range, Position, TextEditor } from "vscode";
+import { TextDocument, Range, Position, TextEditor, Selection } from "vscode";
+import { getExpandCodeList } from "./settings";
 
 export function runEntireFile() {
   const textEditor = vscode.window.activeTextEditor;
@@ -10,28 +11,88 @@ export function runEntireFile() {
 
 export function runInferredCodeBlock() {
   const textEditor = vscode.window.activeTextEditor;
+  console.log('textEditor', textEditor);
   if (!textEditor) {
     return;
   }
 
   const initialCursorPosition = textEditor?.selection.anchor;
+  console.log('initialCursorPosition', initialCursorPosition);
 
   const expandedCodeRange = getExpandedCodeRegion(
     textEditor,
     initialCursorPosition
   );
+  console.log('expandedCodeRange', expandedCodeRange);
+
   const text = textEditor.document.getText(expandedCodeRange);
+  console.log('text', text);
   vscode.commands.executeCommand("jupyter.execSelectionInteractive", text);
+
+  // TODO: move cursor to end of range
+  const endPosition = new Position(expandedCodeRange.end.line + 1, 0);
+  const newSelection = new Selection(endPosition, endPosition);
+  textEditor.selections = [newSelection];
+
 }
 
-function getExpandedCodeRegion(editor: TextEditor, position: Position): Range {
+function getExpandedCodeRegion(
+  editor: TextEditor,
+  initialPosition: Position
+): Range {
   // Assuming that no text is selected
-  const selection = editor.selection;
-  const beginPosition = selection.anchor;
+  const beginRange = new Range(initialPosition, initialPosition);
+  console.log('beginRange', beginRange);
+
+  const initialIndentText = getInitialIndentText(editor, initialPosition);
+  console.log('initialIndentText', initialIndentText);
+
+  const finalRange = expandRangeDownward(editor, beginRange, initialIndentText);
+  console.log('finalRange', finalRange);
+  return finalRange;
+}
+
+function getInitialIndentText(
+  editor: TextEditor,
+  initialPosition: Position
+): string {
+  const lineText = editor.document.lineAt(initialPosition.line).text;
+  const indent = lineText.match(/^\s+/);
+  return indent ? indent[0] : "";
+}
+
+/**
+ * Expand range downwards
+ *
+ * @param editor  [editor description]
+ * @param range   Starting Range
+ * @param indent  Indentation of original line
+ *
+ * @return  {Range}               [return description]
+ */
+function expandRangeDownward(
+  editor: TextEditor,
+  currentRange: Range,
+  indent: string
+): Range {
   const document = editor.document;
+  const expandCodeList = getExpandCodeList();
+  // add whitespace to the list
+  const expandCode = ["\\s"].concat(expandCodeList).join("|");
+  const expandRegex = new RegExp(`^(${indent}(${expandCode})|\s*#|\s*$)`);
 
-  const lastLineNumber = document.lineCount;
+  let nextLineNum = currentRange.end.line;
 
-  const cursorLineText = document.lineAt(beginPosition.line);
+  // expand code to the bottom
+  while (
+    nextLineNum <= editor.document.lineCount &&
+    document.lineAt(nextLineNum).text.match(expandRegex)
+  ) {
+    nextLineNum += 1;
+    console.log('adding a line number')
+  }
+
+  const endPosition = document.lineAt(nextLineNum + 1).range.end;
+  return new Range(currentRange.start, endPosition);
 }
 
