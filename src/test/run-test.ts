11431156d29f2154d142f5c2008be4dd4e017c3a
Kyle Barron <kylebarron2@gmail.com>
2022-11-28 20:54:50 -0700

    set up tests

---
+++ src/test/run-test.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+
+// @ts-expect-error not sure why ts can't find its types
+// It seems to be missing a types field in package.json?
+import { runTests } from '@vscode/test-electron';
+
+async function main() {
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+    // The path to the extension test script
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+  } catch (err) {
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
+}
+
+main();
